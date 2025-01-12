b979d9b80949a68238728e6e34c0bf15091168eb
Wisnu Andika <124800648+pendragonnn@users.noreply.github.com>
2025-01-12 12:33:09 +0700

    fix: [homepage] fix overflow component bug and hide scrollbar

--- src/app/component/Featured.tsx
+++ src/app/component/Featured.tsx
@@ -4,14 +4,17 @@ import React from 'react'
 
 const Featured = () => {
   return (
-    <div className='w-screen overflow-x-scroll text-red-500'>
+    <div className='w-full overflow-x-scroll text-red-500' style={{
+      scrollbarWidth: 'none',
+      msOverflowStyle: 'none'
+    }}>
       <div className="w-max flex">
         {
           featuredProducts.map((item) => (
-            <div key={item.id} className='w-screen h-[60vh] flex flex-col items-center justify-around p-4 hover:bg-fuchsia-50 transition-all duration-300 md:w-[50vw] xl:w-[33vw] xl:h-[90vh]'>
+            <div key={item.id} className='w-screen h-[60vh] flex flex-col items-center justify-around px-4 py-8 hover:bg-fuchsia-50 transition-all duration-300 md:w-[50vw] xl:w-[33vw] xl:h-[90vh]'>
               {/* imgae */}
              {item.img&& (
-              <div className="relative flex-1 w-full hover:rotate-[60deg] transition-all duration-500">
+              <div className="relative flex-1 w-full hover:rotate-[60deg] transition-all duration-500 mb-2">
                 <Image src={item.img} alt='' fill className='object-contain' />
               </div>
               )}
@@ -19,7 +22,7 @@ const Featured = () => {
               <div className="flex-1 flex flex-col items-center justify-center text-center gap-4">
                 <h1 className='text-xl font-bold uppercase xl:text-2xl 2xl:text-3xl'>{item.title}</h1>
                 <p className='p-4 2xl:p-8'>{item.desc}</p>
-                <span className='text-xl font-bold'>{item.price}</span>
+                <span className='text-xl font-bold'>${item.price}</span>
                 <button className='bg-red-500 text-white p-2 rounded-md'>Add to Cart</button>
               </div>
             </div>
--- src/app/component/Slider.tsx
+++ src/app/component/Slider.tsx
@@ -34,7 +34,7 @@ const Slider = () => {
 
   return (
     <div className='flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] lg:flex-row bg-fuchsia-50'>
-      <div className="flex flex-1 items-center justify-center flex-col gap-8 text-red-500 font-bold">
+      <div className="flex flex-1 items-center justify-center flex-col gap-8 text-red-500 font-bold py-4">
         <h1 className='text-5xl text-center uppercase p-4 md:p-10 md:text-6xl lg:text-7xl'>
           {data[currentSlide].title}
         </h1>

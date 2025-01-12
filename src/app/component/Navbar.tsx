c44c8ef57bfa20665869058b9b3cb62f491c64b5
Wisnu Andika <124800648+pendragonnn@users.noreply.github.com>
2025-01-12 12:51:30 +0700

    fix: [ui] fix footer padding, login page, and menu page

--- src/app/component/Navbar.tsx
+++ src/app/component/Navbar.tsx
@@ -26,7 +26,7 @@ const Navbar = () => {
       </div>
 
       <div className="hidden md:flex gap-4 items-center justify-end flex-1">
-        <div className='md:absolute top-3 r-2 lg:static flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md'>
+        <div className='md:absolute top-3 r-2 lg:static flex items-center gap-2 cursor-pointer bg-orange-300 px-2 py-1 rounded-md'>
           <Image src={"/phone.png"} alt='' width={20} height={20}/>
           <span>123 455</span>
         </div>
--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -7,11 +7,11 @@ const MenuPage = () => {
     <div className='px-4 md:py-8 lg:px-20 xl:px-40 h-full flex flex-col md:flex-row items-center'>
       {
         menu.map(category => (
-          <Link href={category.slug} key={category.id} className='w-full md:w-1/2 h-1/3 bg-cover p-8' style={{backgroundImage: `url(${category.img})`}}>
+          <Link href={category.slug} key={category.id} className='w-full md:h-[50vh] md:w-1/2 bg-cover p-8' style={{backgroundImage: `url(${category.img})`}}>
             <div className={`text-${category.color} w-1/2 `}>
               <h1 className='uppercase font-bold text-3xl'>{category.title}</h1>
               <p className='text-sm my-8'>{category.desc}</p>
-              <button className={`hidden 2xl:block bg-red-500 text-white py-2 px-4 rounded`}>Explore</button>
+              <button className={`hidden 2xl:block font-bold ${category.color == "black" ? "bg-black text-white" : "bg-white text-red-500"} py-2 px-4 rounded`}>Explore</button>
             </div>
           </Link>
         ))

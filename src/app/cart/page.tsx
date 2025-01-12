715691e2e81234a31161c723e810cc707e7ef3c2
Wisnu Andika <124800648+pendragonnn@users.noreply.github.com>
2025-01-12 10:12:19 +0700

    feat: add cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,8 +1,45 @@
+import Image from 'next/image'
 import React from 'react'
 
 const CartPage = () => {
   return (
-    <div>CartPage</div>
+    <div className='h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col text-red-500 lg:flex-row'>
+      {/* product container */}
+      <div className='h-1/2 p-4 flex flex-col justify-center overflow-scroll lg:h-full lg:w-2/3 2xl:w-1/2 lg:px-20 xl:px-40'>
+        {/* single item */}
+        <div className='flex items-center justify-between mb-4'>
+          <Image src={"/temporary/p1.png"} alt='' width={100} height={100}/>
+          <div>
+            <h1 className='uppercase text-xl font-bold'>Sicilian</h1>
+            <span>Large</span>
+          </div>
+          <h2 className='font-bold'>$79.90</h2>
+          <span className='cursor-pointer'>X</span>
+        </div>
+      </div>
+
+      {/* payment container */}
+      <div className='h-1/2 p-4 bg-fuchsia-50 flex flex-col gap-4 justify-center lg:h-full lg:w-1/3 2xl:w-1/2 lg:px-20 xl:px-40 2xl:text-xl 2xl:gap-6'>
+        <div className='flex justify-between'>
+          <span>Subtotal (3 items)</span>
+          <span>$81.70</span>
+        </div>
+        <div className='flex justify-between'>
+          <span>Service Cost</span>
+          <span>$0.00</span>
+        </div>
+        <div className='flex justify-between'>
+          <span>Delivery Cost</span>
+          <span className='text-green-500'>FREE</span>
+        </div>
+        <hr className='my-2' />
+        <div className='flex justify-between'>
+          <span>Total (INCL. VAT)</span>
+          <span className='font-bold'>$81.70</span>
+        </div>
+        <button className='bg-red-500 text-white p-3 rounded-md w-1/2 self-end'>CHECKOUT</button>
+      </div>
+    </div>
   )
 }
 
